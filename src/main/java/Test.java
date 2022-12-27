2cb11dcb18428dc35a2d6dc85e1674a20f818ab3
wuhuqifei <1214086027@qq.com>
2022-12-27 22:00:04 +0800

    test

---
+++ src/main/java/Test.java
@@ -0,0 +1,15 @@
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * @author Administrator
+ * @version 1.0
+ * @program: LeetCode
+ * @description: mapTest
+ * @date 2022/12/27 21:37
+ */
+public class Test {
+    public  static void main(String[] args) {
+        ConcurrentHashMap<String,String> concurrentHashMap = new ConcurrentHashMap<>();
+        concurrentHashMap.put("1","1");
+    }
+}
