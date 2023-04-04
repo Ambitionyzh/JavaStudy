b424d3a52bc45b5cf7f0d231eca65b0b9a37f8de
wuhuqifei <1214086027@qq.com>
2023-04-05 00:24:54 +0800

    JVM

---
+++ src/main/java/com/Jvm/Math.java
@@ -0,0 +1,29 @@
+package com.Jvm;
+
+import com.mybatisDemo.User;
+
+/**
+ * @author yongzh
+ * @version 1.0
+ * @program: JavaTest
+ * @description:
+ * @date 2023/3/31 21:34
+ */
+public class Math {
+
+    public static final int initData = 666;
+    public static User user = new User();
+
+    public int computer(){
+        int a= 1;
+        int b = 2;
+        int c = (a + b) *10;
+        return  c;
+
+    }
+    public static void main(String[] args) {
+        Math math = new Math();
+        math.computer();
+        System.out.println();
+    }
+}
